1989cc631bbe9a4bf993fc919da96a413828e9f5
Xiaobo Fu <xiaobofu.maps@gmail.com>
2024-02-08 17:34:37 +0000

    Update HEM.cs

--- week 5/HEM.cs
+++ week 5/HEM.cs
@@ -3,50 +3,55 @@ using System.Collections.Generic;
 using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.ProBuilder;
 
 public class HEM : MonoBehaviour
 {
+    // plgu in a Unity object that we want to get mesh from
     [SerializeField] Object gameObject;
 
     // a haldedge mesh 
     private HalfedgeMesh hem;
 
+    // a input unity mesh we get from the gameObject
     private Mesh inputMesh;
 
-    // variables to hold Unity mesh, vertices and tris
-    private Mesh mesh;
-    private Vector3[] vertices;
-    private int[] triangles;
+    // variables to hold output Unity mesh
+    private Mesh outputMesh;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // get the inputMesh from the game object
         inputMesh = gameObject.GetComponent<MeshFilter>().mesh;
-        // create a tri with three given point position
+        // create a halfdege mesh according to the inputMesh
         hem = new HalfedgeMesh();
         hem.ConvertFromUnityMesh(inputMesh);
 
-        // plugin the unity mesh we are going to create
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        // set the output mesh
+        outputMesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = outputMesh;
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateMesh();
+        UpdateOutputMesh();
+
+        // here we can do some mesh processing
+        // hem.MeshProcessing()
+
+        // update hem incase the input changes
         hem.ConvertFromUnityMesh(inputMesh);
     }
 
-    void UpdateMesh()
+    void UpdateOutputMesh()
     {
-        mesh.Clear();
+        outputMesh.Clear();
 
-        mesh.vertices = hem.GetVertices();
-        mesh.triangles = hem.GetTriangles();
+        outputMesh.vertices = hem.GetVertices();
+        outputMesh.triangles = hem.GetTriangles();
         // update normals for rendering 
-        mesh.RecalculateNormals();
+        outputMesh.RecalculateNormals();
     }
 }
